3ba017135484049480ff7167e054e1526399ed8e
MrTat-9921 <52516762+MrTat-9921@users.noreply.github.com>
2019-08-20 18:13:17 +0700

    Add files via upload

---
+++ html/test.js
@@ -0,0 +1,15 @@
+$(
+    function(){
+        $(window).scroll(
+            function () {
+                if ($(this).scrollTop() > 100) $(".lentop").fadeIn();
+                else $(".lentop").fadeOut();
+            }
+        );
+        $(".lentop").click(
+            function () {
+            $("body,html").animate({scrollTop: 0}, "slow");
+            }
+        );
+    }
+);
---
+++ html/vietbao.js
@@ -0,0 +1,60 @@
+
+
+
+
+
+var news =[
+    {
+        name: 'Thư - Vũ bất ngờ tái hợp, song ca cực ngọt trước ngày kết thúc Về nhà đi con',
+        picture: 'https://vnn-imgs-f.vgcloud.vn/2019/08/10/17/-27.jpg',
+        content:['Diễn viên Bảo Thanh bất ngờ có mặt trong buổi họp fan tại Hà Nội của Quốc Trường chiều 10/8. Cặp đôi diễn lại một trích đoạn trong phim "Về nhà đi con" và lần đầu hát chung. ','Chiều 10/8, Quốc Trường tổ chức gặp fan tại Hà Nội. Đây là lần đầu tiên nam diễn viên Cần Thơ tổ chức gặp fan tại Hà Nội, nơi anh gắn bó suốt 6 tháng qua vì phim "Về nhà đi con". Nam diễn viên xuất hiện chỉn chu trong trang phục quần âu và sơ mi đen - vest trắng và khiến fan bất ngờ khi hát ca khúc "Tình khúc vàng" đình đám một thời của thần tượng Đan Trường. '],
+        tag: 0,
+    },
+    {
+        name: 'Ảnh hiếm về sự tàn phá của bom hạt nhân ở Nagasaki',
+        picture: 'https://vnn-imgs-f.vgcloud.vn/2019/08/10/11/buc-anh-mau-hiem-ve-su-tan-pha-kinh-hoang-cua-bom-hat-nhan-my-o-nagasaki.jpg',
+        content:['fjiwhfewhfewhfwehifewhiefcafhfuwe'],
+        tag:1,
+    },
+    {
+        name: 'a1',
+        picture: './phonepicutres-TA.jpg',
+        content:['fjiwhfewhfewhfwehifewhiefcafhfuwe'],
+        tag:2,
+    },
+    {
+        name: 'a2',
+        picture: './phonepicutres-TA.jpg',
+        content:['fjiwhfewhfewhfwehifewhiefcafhfuwe'],
+        tag:3,
+    },
+    {
+        name: 'a3',
+        picture: './phonepicutres-TA.jpg',
+        content:['fjiwhfewhfewhfwehifewhiefcafhfuwe'],
+        tag:4,
+    },
+    {
+        name: 'a4',
+        picture: './phonepicutres-TA.jpg',
+        content:['fjiwhfewhfewhfwehifewhiefcafhfuwe'],
+        tag:5
+    },
+]  
+var currenttag=5 
+function addNews(x){
+    var NewsHTML = `
+    <div class='new'>
+    <img src="${news[x].picture}" id='picture' height=200 width=320px>
+    <a href=''./${news[x].tag} id='title'>${news[x].name}</a>
+    <p id='description'>${news[x].content[0]}</p>
+    </div>
+    `  
+    var contents = document.getElementById('content')
+    contents.insertAdjacentHTML('beforeend',NewsHTML)
+}
+for(let i = 0; i<news.length-4;i++){
+    addNews(i)
+};
+
+
