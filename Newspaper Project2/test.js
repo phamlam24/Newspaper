58c28d10881f93e321c48353f4ce29cd65c8767a
MrTat-9921 <52516762+MrTat-9921@users.noreply.github.com>
2019-08-24 21:21:21 +0700

    Add files via upload

---
+++ Newspaper Project2/test.js
@@ -0,0 +1,25 @@
+function fetchData() {
+    sendGetRequest('https://5d4ebd08d3acc30014f3889a.mockapi.io/testApi',function(ResponseData){
+        function addNews(x){
+            var titleandpic =`
+            <h1 id='title_posts'>${news[x].name}</h1>
+            <hr>
+            <img src='${news[x].picture}' id='picture'>
+            `
+            document.getElementById('root').insertAdjacentHTML('beforeend',titleandpic)
+            var quick_info = `<div id='quick_info'>${news[x].content[0]}</div>`
+            document.getElementById('root').insertAdjacentHTML('beforeend',quick_info)
+            for(let i = 1; i<news[x].content.length;i++){
+                var paragraph =`
+                <p>${news[x].content[i]}</p>
+                `
+                document.getElementById('root').insertAdjacentHTML('beforeend',paragraph)
+            }
+        }
+        var news = ResponseData
+        console.log(news)
+        addNews(localStorage.getItem('tag'))
+
+    })
+}
+fetchData()
